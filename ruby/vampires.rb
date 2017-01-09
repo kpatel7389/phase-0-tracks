92cbd773aaee6059830fcc048ebe23d80129c8bb
Kosha Patel <kpatel7389@gmail.com>
2017-01-09 11:56:04 -0600

    Add loop to checkout allergies

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,16 @@
-puts What is your name?
+puts "How many employes will be processed?" 
+employes = gets.chomp
+
+puts "What is your name?"
 vampire_name = gets.chomp
 
-puts How old are you? What year were you born in?
+puts "How old are you? What year were you born in?"
 age = gets.chomp
 
-puts Our company cafeteria serves garlic bread. Should we order you some?
+puts "Our company cafeteria serves garlic bread. Should we order you some?"
 garlic = gets.chomp
 
-puts Would you like to enroll the company’s health insurance?
+puts "Would you like to enroll the company’s health insurance?"
 insurance = gets.chomp
 
 
@@ -16,9 +19,9 @@ vampire_name = true
 insurance = true
 age = true
 
-if #{age} && #{garlic} || #{insurance} = true 
+if #{age} && #{garlic} || #{insurance} == true 
 	puts "Probably not a vampire."
-elsif #{age} && #{garlic} && #{insurance} = false
+elsif #{age} && #{garlic} && #{insurance} == false
 	puts "Almost certainly a vampire"
 else 
 	puts "Proabaly a vampire"
@@ -26,10 +29,27 @@ else
 end 
 
 
-if #{vampire_name} = "Drake Cula" || "Tu Fang"
+if #{vampire_name} == "Drake Cula" || "Tu Fang"
 	puts "Defintely a vampire"
 else
 	"Results inconclusive"
 
 end 
+
+def type_of_allergies(allergies)
+	i = 0
+	
+	while i < allergies
+		 
+		 if allergies == sunshine
+		 	puts "Probably a vampire"
+		 end
+		
+		i += 1	
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends." 
+
+
+
 
