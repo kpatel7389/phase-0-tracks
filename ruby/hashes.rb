9f37ddc90859e6ab85547d6a9a14728ea3485689
Kosha Patel <kpatel7389@gmail.com>
2017-01-11 19:53:57 -0600

    Add input information/details

---
+++ ruby/hashes.rb
@@ -0,0 +1,20 @@
+puts "What is your name?"
+names = gets.chomp
+
+puts "What is your address?"
+address = gets.chomp
+
+puts "What is your age?"
+age = gets.chomp.to_i
+
+puts "How many children do you have?"
+kids = gets.chomp
+
+puts "Do you have a theme in mind? (y/n)"
+theme = gets.chomp
+
+puts "Do you like wallpaper? (y/n)"
+wallpaper = gets.chomp
+
+puts "Do you prefer dark colors or lighter colors?"
+paint_color = gets.chomp
