32fbb60e737d50ac21f63ebee8e7c6719a5ac178
Kosha Patel <kpatel7389@gmail.com>
2017-01-14 11:30:48 -0600

    Add until loop to create interface to get repeated output of fake name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,13 +1,27 @@
 def next_vowel(word)
   vowels = ["a", "e", "i", "o", "u"]
   word = word.split('')
-  word_new = word.map do |char|
+  word_new = word.map do|char| 
+
     if vowels.include?(char)
-      vowels.rotate(1)[vowels.index(char)]
+      vowels.rotate(1)[vowels.index(char)]	
     else
-      char
+    	char.next
     end
-  end
+   end
   puts word_new.join
 end
-next_vowel('kosha')
+#next_vowel('Felicia Torres')
+
+
+real_name = 0
+    until ["quit"].include? real_name do
+        puts "What is your name" 
+        real_name = gets.chomp
+    
+    if real_name == "quit"    
+        puts "The end" 
+    else   
+        puts next_vowel(real_name) 
+    end
+end 
