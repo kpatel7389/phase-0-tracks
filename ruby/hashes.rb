0ae92ed291ed6aff7eae6b9b4ba34bd355c75bfd
Kosha Patel <kpatel7389@gmail.com>
2017-01-16 11:19:46 -0600

    add data input into hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,10 +2,18 @@
 
 
 
-loop do
+user_data = {}
+names = ''
+address = ''
+age = ''
+kids = ''
+theme = ''
+bedrooms = ''
 
+
+while 
 puts "What is your name?"
-names = gets.chomp
+input names = gets.chomp
 
 puts "What is your address?"
 address = gets.chomp
@@ -22,10 +30,17 @@ theme = gets.chomp
 
 puts "How many bedrooms do you have?"
 bedrooms = gets.chomp.to_i
+ 
 
-input = gets.chomp
-break if input == 'exit'
-end 
+puts "Here is all the information:"
 
-user_data = {first:names, address:address, age:age, kids:kids, theme:theme, bedrooms:bedrooms}
-puts "#{user_data}"
+user_data.hash.each { |key,value|
+	puts "#{key}=> #{value}"
+
+}
+
+#user_data = {first:names, address:address, age:age, kids:kids, theme:theme, bedrooms:bedrooms}
+#puts "#{user_data}"
+
+
+
