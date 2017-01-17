e77b44552215ef58a8186976d2eb0b537859dc7a
Kosha Patel <kpatel7389@gmail.com>
2017-01-17 07:11:42 -0600

    Print a few ways to access data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,17 +4,24 @@ holiday_supplies = {
     New_Years: "champagne glasses"
   },
   summer: {
-    July_Fourth:['BBQ', 'Flags', 'Fireworks']
+    July_Fourth:['BBQ', 'Flags', 'Fireworks'],
     BBQ_Items: {
       Meat: ['Sausage', 'Hot Dogs'],
       Vegetarian:[]
     }
   },
   spring: {
-    Memorial_Day: ['BBQ', 'Veterns']
+    Memorial_Day: ['BBQ', 'Veterns'],
     Easter: ['Bunny', 'Eggs', 'Peeps']
   },
   fall: {
     Labor_Day: "hot dogs"
   }
 }
+
+#p holiday_supplies[:winter][:Christmas][0]
+
+#p holiday_supplies[:summer][:BBQ_Items][:Vegetarian].push('Corn', 'Potatoes')
+
+
+p holiday_supplies[:summer][:July_Fourth].reverse
