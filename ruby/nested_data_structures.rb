c5e857fe36017e8b0bf842f672cadcbbeaf2d860
Kosha Patel <kpatel7389@gmail.com>
2017-01-17 07:01:06 -0600

    Build a data structure of nested arrays and hashes

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,20 @@
+holiday_supplies = {
+  winter: { 
+    Christmas: ["lights", "tree"],
+    New_Years: "champagne glasses"
+  },
+  summer: {
+    July_Fourth:['BBQ', 'Flags', 'Fireworks']
+    BBQ_Items: {
+      Meat: ['Sausage', 'Hot Dogs'],
+      Vegetarian:[]
+    }
+  },
+  spring: {
+    Memorial_Day: ['BBQ', 'Veterns']
+    Easter: ['Bunny', 'Eggs', 'Peeps']
+  },
+  fall: {
+    Labor_Day: "hot dogs"
+  }
+}
