775078b2a3df23b260402c34b130e2e70f110670
Kosha Patel <kpatel7389@gmail.com>
2017-01-19 20:45:29 -0600

    add getter and setter methods and test with driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,19 +7,46 @@ class Santa
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!" 
 	end
-
-	def initialize(gender, ethnicity)
-		@gender = gender
-		@ethnicity = ethnicity
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
-		puts "Intializing Santa instance..."
+	
+	def age
+        puts "The santa" + " is now #{@age} years old."
+        @age = @age
+        oneYearPasses
+      end
+      
+  	def oneYearPasses
+        if @age >= 0
+          @age = @age + 1
+        end
+  	
+  	
+  	def gender
+    @gender
+  	end
+  
+	def gender=(new_gender)
+	  @gender = new_gender
 	end
 
-	def 
-		
+	def initialize()
+		@gender = ''
+		@ethnicity = ''
+		@age = 0
+	end
+	
+	def ethnicity
+	  @ethnicities
+	end
 end
 
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+santas = Santa.new
+santas.age
+santas.age
+santas.gender = "The gender that was agender"
+puts "#{santas.gender} is now a female"
+santas.speak
 
 santas = []
 genders = ['agender', 'female', 'bigender', 'male', 'female', 'gender fulid', 'N/A']
@@ -27,10 +54,10 @@ ethnicities = ['black', 'Latino', 'white', 'Japanese', 'prefer not to say', 'Mys
 
 puts "Iterating through list to add gender and ethnicity ..."
 genders.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
+  santas << Santa.new()
   puts "Santas gender is #{genders[i]} and the ethnicity is #{ethnicities[i]} "
 end
+  
 
-santa.speak
-santa.eat_milk_and_cookies("Chocolate Chip")
 
+  
