155d0c568a24ba4f67d5950b024bc90570e52ccc
Kosha Patel <kpatel7389@gmail.com>
2017-01-19 19:05:27 -0600

    iterate through santas gender and ethnicity and print it out

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,10 +15,22 @@ class Santa
 		@age = 0
 		puts "Intializing Santa instance..."
 	end
+
+	def 
 		
 end
 
-santa = Santa.new 
+
+santas = []
+genders = ['agender', 'female', 'bigender', 'male', 'female', 'gender fulid', 'N/A']
+ethnicities = ['black', 'Latino', 'white', 'Japanese', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
+
+puts "Iterating through list to add gender and ethnicity ..."
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicities[i])
+  puts "Santas gender is #{genders[i]} and the ethnicity is #{ethnicities[i]} "
+end
 
 santa.speak
 santa.eat_milk_and_cookies("Chocolate Chip")
+
