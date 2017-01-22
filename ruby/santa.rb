aa5aa95d656bf6620d23b6eac3eac30e05782a27
Kosha Patel <kpatel7389@gmail.com>
2017-01-22 14:48:05 -0600

    Add driver code to print mulitple santas between the ages of 0 to 140

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,60 +1,62 @@
 class Santa
-	attr_reader :ethnicity, :age
-	attr_accessor :age, :reindeer, :gender
+   attr_reader  :ethnicity , :age
+   attr_accessor :age, :reindeer, :gender
+ 
+ 
+   def initialize(gender, ethnicity)
+     puts "Initializing Santa instance now..."
+    
+     @gender = gender
+     @ethnicity = ethnicity
+     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+     @age = 0
+   end  
+  
+  	def celebrate_birthday
+ 		  @age += 1  
+ 	end
 
-	def initialize(gender, ethnicity)
-		@gender = gender
-		@ethnicity = ethnicity
-		@age = 0
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	end
+ 	def santas_age
+ 		puts "This santa is  #{@age} years old."
+ 	end
 
-	def speak()
+
+	def speak
 		puts "Ho, ho, ho! Happy holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type} cookie!" 
 	end
-	
-	def age
-        @age += 1
-      end
-      
-  	def celebrate_birthday
-  		puts "This santa is #{age} years old"
+end
   
   	
-  
-	def rand_genders
-		genders = ['agender', 'female', 'bigender', 'male', 'female', 'gender fulid', 'N/A']
-		gender = genders[rand(genders.length)]
-	end
+ 	def rand_gender
+     genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+     gender = genders[rand(genders.length)]
+ 	end
 	
-	def rand_ethnicity
-		ethnicities = ['black', 'Latino', 'white', 'Japanese', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
-		ethnicity = ethnicities[rand(ethnicities.length)]
-	end
-end
+	 def rand_ethnicity
+     ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+     ethnicity = ethnicities[rand(ethnicities.length)]
+ 	end
 
 
-santas = Santa.new
-santas.age
-santas.age
-santas.gender = "The gender that was agender"
-puts "#{santas.gender} is now a female"
-santas.speak
-
-santas = []
-
-ethnicities = ['black', 'Latino', 'white', 'Japanese', 'prefer not to say', 'Mystical Creature (unicorn)', 'N/A']
-
-puts "Iterating through list to add gender and ethnicity ..."
-genders.length.times do |i|
-  santas << Santa.new()
-  puts "Santas gender is #{genders[i]} and the ethnicity is #{ethnicities[i]} "
-end
+ santas = []
+  4.times do |a|
+    santa = Santa.new(rand_gender, rand_ethnicity,)
+    santa.age = rand(140)
+    
+ 
+   puts "#{santa.speak}"
+   puts "#{santa.santas_age}"
+   puts "This Santa ##{a+1} attributes are:"
+   puts "This Santa's Age: #{santa.age}"
+   puts "This Santa's Gender: #{santa.gender}"
+   puts "This Santa's Ethnicity: #{santa.ethnicity}"
+   puts "#{santa.eat_milk_and_cookies("snickerdoodle")}"
   
+ end
 
 
   
