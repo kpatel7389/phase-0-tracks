8a4c6d4b53bbbe79066afb2dbad86ce2f6eeaac4
Kosha Patel <kpatel7389@gmail.com>
2017-01-24 14:11:37 -0600

    Explained what require_relative is in comment, comment above each method defining what it is doing, come up with a driver code to get an output of all 50 states, refactor predicted_deaths and speed_spread methods.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -4,58 +4,64 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# allows you to get access to the data in the file using the relative path
 require_relative 'state_data'
 
 class VirusPredictor
-
+  attr_reader :population_density, :population
+  #This method gives values to the specified attributes based on the parameters given 
+  #for a new instances of the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #This method calls the two methods at the same time
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  #This method determines the number of deaths in a state based on population density and total population size
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+    if population_density >= 200
+      rate = 0.4
+    elsif population_density >= 150
+      rate = 0.3
+    elsif population_density >= 100
+      rate = 0.2
+    elsif population_density >= 50
+      rate = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      rate = 0.05
     end
+
+    number_of_deaths = (population * rate).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #This method determines the speed the disease will spread based on population density
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
+
+    if population_density >= 200
+      speed = 0.5
+    elsif population_density >= 150
+      speed = 1
+    elsif population_density >= 100
+      speed = 1.5
+    elsif population_density >= 50
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -69,8 +75,15 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+STATE_DATA.each do |key, value|
+state = VirusPredictor.new(key, value[:population_density], value[:population])
+state.virus_effects
+end
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+
+
+
+/alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
 
 jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
@@ -80,7 +93,7 @@ california = VirusPredictor.new("California", STATE_DATA["California"][:populati
 california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+alaska.virus_effects/
 
 
 #=======================================================================
