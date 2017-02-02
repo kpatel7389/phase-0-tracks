b72e5271e5e3fd59cf619491fbe1c9bfb1e26db0
Kosha Patel <kpatel7389@gmail.com>
2017-02-02 14:57:38 -0600

    Allow user to enter as many names until the user types 'quit'. Once done the console prints out their real name and the fake name generated

--- ruby/alias_manager_Frog.rb
+++ ruby/alias_manager_Frog.rb
@@ -1,28 +1,47 @@
-#Method to return the next vowel character
+#Method to return the next vowel and consonant character
 def secret_name(word)
 vowels = ['a', 'e', 'i', 'o', 'u']
 consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-
+ #Change the string into an array
 word = word.split('')
-
+#iterate through each letter of the string
 word_new = word.map do |letter|
+  #if letter includes a vowel then change to the next vowel
   if vowels.include?(letter)
     vowels.rotate(1)[vowels.index(letter)]
+  #if letter includes consonant then change to the next consonant
   elsif consonants.include?(letter)
     consonants.rotate(1)[consonants.index(letter)]
+  #if there is a space, keep it a space
   elsif letter == " "
     letter = " "
   else
     letter.next
   end
 end
-puts word_new.join
+#convert array back to string
+ word_new.join
 end
 
-secret_name('Felicia Torres')
+#Driver Code
+#secret_name('Felicia Torres')
  
  
+name_list = {}
+real_name = ''
 
+loop do
+  puts "Enter first and last name. Type 'quit' when done"
+  real_name = gets.chomp
+  if real_name == 'quit' || real_name == ''
+    break
+  end
+ name_list[real_name] = secret_name(real_name)
+end
 
+puts 'Here is the list of your alias names!'
+name_list.each do |name, fake_name|
+  puts "#{name} is also known as #{fake_name}!"
+end
 
 
