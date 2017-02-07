e66674af46d94df5f5331d6fc9072d386ddd13fe
Kosha Patel <kpatel7389@gmail.com>
2017-02-07 17:04:25 -0600

    Release0: create Santa class. Add 3 instance methods of speak, eat_milk_and_cookies that takes a cookie type, and initialize.  Add driver code to test

---
+++ ruby/santa_frog.rb
@@ -0,0 +1,16 @@
+class Santa
+
+  def initialize
+    puts "Initializing Santa instance..."
+
+  def speak
+    puts "Ho, ho, ho! Happy Holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type} cookie!"
+  end
+end
+
+
+  
