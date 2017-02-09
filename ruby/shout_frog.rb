c63077c27b7b6beda6b518f9c5a2692f7f6b731c
Kosha Patel <kpatel7389@gmail.com>
2017-02-08 21:12:00 -0600

    Release3: comment out old code. Add mixin version of shout module. Add 2 classes: Birthday and Stop and include shout module. Add driver code

--- ruby/shout_frog.rb
+++ ruby/shout_frog.rb
@@ -1,17 +1,31 @@
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		puts words + '!!!' + ':('
 	end
 
-	def self.yellling_happily(words)
+	def yellling_happily(words)
 		puts words + '!!!' + ':D'
 	end
-
 end
 
+class Stop
+	include Shout
+end
 
-Shout.yellling_happily("I like puppies")
-Shout.yell_angrily("I don't like carrots")
+class Birthday
+	include Shout
+end
+
+birthday = Birthday.new
+birthday.yellling_happily('Happy Birthday')
+
+stop = Stop.new
+stop.yell_angrily("No")
+
+
+
+# Shout.yellling_happily("I like puppies")
+# Shout.yell_angrily("I don't like carrots")
 
 
 
