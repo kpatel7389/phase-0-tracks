c1dfa98b2c4d43343390689121606688c7a63052
Kosha Patel <kpatel7389@gmail.com>
2017-02-15 08:37:15 -0600

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// Create a function that takes a string as a parameter
+	//set var to an empty string
+	//use for loop
+		// index is equal to length of string -1
+		// decrement and until index is greater than 0
+	// return output
+// call the function reverse with a string 
+
+
+
