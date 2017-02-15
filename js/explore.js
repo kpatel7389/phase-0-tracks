e002fe849352d465dbe38fefe7896edae38a902c
Kosha Patel <kpatel7389@gmail.com>
2017-02-15 08:44:12 -0600

    Add code according to pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,10 +1,23 @@
 // Create a function that takes a string as a parameter
-	//set var to an empty string
+	//set string to an empty string
+	//set index equal to length of string -1
 	//use for loop
 		// index is equal to length of string -1
 		// decrement and until index is greater than 0
 	// return output
 // call the function reverse with a string 
 
+function reverse(str) {
+
+var string = " "
+var index = str.length -1
+for (var i = index; i >= 0; i--) {
+	string += str[i]
+}
+
+return string
+}
+
+reverse("hello")
 
 
