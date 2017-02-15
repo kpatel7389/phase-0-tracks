79af63f590a61cb1aa12b5cf1b17e8b444823324
Kosha Patel <kpatel7389@gmail.com>
2017-02-15 09:21:01 -0600

    Add pseudocode

---
+++ js/algos_frogs.js
@@ -0,0 +1,12 @@
+// Create a function that takes an array as a parameter
+// Set var to what will be returned
+// Use for loop to through the array until it finds the longest phrase
+// 	IF the current phrase in the array is longer than the next 
+// 		then store the longest phrase in the array
+// return the var where the longest phrase is stored
+// call the function
+
+
+
+
+
